ff0bf41a38c67298bca74e0704d624d95a6bf3df
SURAJ PANDEY <99715986+Suraj-360@users.noreply.github.com>
2024-09-15 01:58:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./routes/route');
 
 // CORS Middleware
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.setHeader("Access-Control-Allow-Origin", '*');
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     
