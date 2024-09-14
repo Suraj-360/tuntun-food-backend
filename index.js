91b55fead28c069e631f968943724b6c77a1bd0d
SURAJ PANDEY <99715986+Suraj-360@users.noreply.github.com>
2024-09-15 01:05:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 // CORS Middleware
 app.use((req, res, next) => {
-    res.setHeader(`Access-Control-Allow-Origin", "${FRONTEND_URL}`);
+    res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     
